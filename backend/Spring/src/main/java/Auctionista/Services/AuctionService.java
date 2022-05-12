0e4c63b09f6063fb139a87197a7577f41a5cd25e
Albinhsn <albin.henningsson1@gmail.com>
2022-05-12 09:03:58 +0200

    start auth

--- backend/Spring/src/main/java/Auctionista/Services/AuctionService.java
+++ backend/Spring/src/main/java/Auctionista/Services/AuctionService.java
@@ -5,9 +5,8 @@ import java.util.Date;
 import java.util.List;
 
 import org.bson.types.ObjectId;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
 import Auctionista.Dto.AuctionDto;
