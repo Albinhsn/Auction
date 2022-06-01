2ab467ae45505fd52e374eec9a1dfd2ba68815bd
Albinhsn <albin.henningsson1@gmail.com>
2022-06-01 15:15:40 +0200

    removed a lorem

--- frontend/src/Components/Homepage/Jumbo.js
+++ frontend/src/Components/Homepage/Jumbo.js
@@ -5,8 +5,8 @@ export default function Jumbo() {
   return (
     <div className="jumbotron jumbotron-fluid text-center">
       <div className="container">
-        <h1 className="display-3 text-white">Arla was here</h1>
-        <p className="lead text-white">Lorem Lorem Lorem Lorem Ipsum Lorem Lorem Lorem Lorem</p>
+        <h1 className="display-3 text-white">Auctionista</h1>
+        <p className="lead text-white">Lorem Lorem Lorem Ipsum Lorem Lorem Lorem Lorem</p>
       </div>
   </div>
   )
